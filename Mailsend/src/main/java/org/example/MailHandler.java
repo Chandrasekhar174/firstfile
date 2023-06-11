b4259a3aec4234cd174f5ecc397a37c515a4fe1b
Chandhu <chandrasekharsahu174@gmail.com>
2023-06-11 17:14:12 +0530

    sendmail file

---
+++ Mailsend/src/main/java/org/example/MailHandler.java
@@ -0,0 +1,53 @@
+package org.geekster;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import java.io.File;
+import java.util.Properties;
+
+public class MailHandler {
+
+    void sendMail()
+    {
+        Properties sysProperties = System.getProperties(); //this gives me a hash-map/ hash-table
+
+        System.out.println(sysProperties);
+
+        sysProperties.put("mail.smtp.host",MailMetaData.HostServer);
+        sysProperties.put("mail.smtp.port",MailMetaData.port);
+        sysProperties.put(MailMetaData.sslProperty,"true");
+        sysProperties.put(MailMetaData.authPerm,"true");
+
+
+        //create a session using sender-email and password
+        Authenticator mailAuthenticator = new CustomizedMailAuthentication();
+        Session mailSession = Session.getInstance(sysProperties,mailAuthenticator);
+
+        //mime message build
+
+        MimeMessage mailMessage = new MimeMessage(mailSession);
+
+        try {
+            mailMessage.setFrom(MailMetaData.myUserMail);
+            mailMessage.setSubject("This is my java code testing");
+            mailMessage.setText("Hey this is chandrasekhar who is trying to send mail using java");
+            mailMessage.setText("hello bro");
+
+            //set the receiver
+
+            Address receiverEmail = new InternetAddress(MailMetaData.receiverMail);
+            mailMessage.setRecipient(Message.RecipientType.TO, receiverEmail);
+
+            Transport.send(mailMessage);
+        }
+        catch(Exception mailException)
+        {
+            System.out.println(mailException.toString());
+        }
+
+
+
+    }
+}
---
+++ Mailsend/src/main/java/org/example/MailMetaData.java
@@ -0,0 +1,17 @@
+package org.geekster;
+
+public class MailMetaData {
+
+    public static final String HostServer = "smtp.gmail.com";
+    public static final String port = "465";
+    public static final String sslProperty = "mail.smtp.ssl.enable";
+    public static final String authPerm = "mail.smtp.auth";
+
+    public static final String myUserMail = "chandrasekharsahu174@gmail.com";
+
+    public static final String password = "jpjoqoppvvpcomdy";// app password
+
+    public static final String receiverMail = "chandrasekharsahu171@gmail.com";
+
+
+}
